7bcfb6ed190e2a88f5029948d0f96c9686fd3152
Dr. Gifter <phamtuankiet1774@gmail.com>
2025-06-27 00:47:16 +0700

    Add files via upload

---
+++ style/script.js
@@ -0,0 +1,19 @@
+const sound = document.getElementById("scarySound");
+const scaryImage = document.getElementById("scaryImage");
+let loopStartTime = 0.5;
+
+function blowCandle() {
+  const candle = document.getElementById("candleImage");
+  candle.src = "./style/Candle-Off.png";
+
+  sound.volume = 1.0;  
+  sound.currentTime = loopStartTime;
+  sound.play();
+
+  scaryImage.style.display = "block"; 
+
+  sound.onended = function () {
+    sound.currentTime = loopStartTime;
+    sound.play();
+  };
+}
